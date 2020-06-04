cd06b2c64b30b2e55ca84086e7de37927e1defb1
Vlad Munteanu <munteanuv@gmail.com>
2020-06-04 11:37:04 -0700

    load, hash, check drafts

--- dictionary.c
+++ dictionary.c
@@ -35,34 +35,25 @@ bool load(const char *dictionary)
         return false;
     }
 
-    /* //determine size of dictionary file */
-    fseek(dict_ptr, 0, SEEK_END);
-    int dict_size = ftell(dict_ptr);
-    rewind(dict_ptr);
 
-    /* allocate memory for each node */
-    node *dict_live = malloc(dict_size * sizeof(int));
+    /* allocate (1) memory for each new word (will use the same block) and 
+    (2) memory for each new node (will add a new node block for each word)  */
+    /* TO CONFIRM: Can i get away with not using the word block and scanning directly into the node */
+    char *word = malloc(LENGTH * sizeof(char)); /* TO CONFIRM: can i get away with the one malloc and the loop replacing the word each time?  */
+    node *w = malloc(sizeof(node));
     
-    /* scan dictionary into allocated array */
-    if(fscanf(dict_ptr, "%s", dict_live) == NULL) 
+    /* scan dictionary into hash table one word at a time */
+    while (fscanf(dict_ptr, "%s", word) != EOF)
     {
-        printf("Couldn't copy dictionary into a dynamic array...");
-        return false;
-    }
-    else
-    {
-        return true;
-    }
-    
-    int index = 0;
-    while (dict_live != NULL){
-        node *w = malloc(sizeof(node));
-        strcpy(w->word, dict_live[index]); /* NEED TO FIX HOW DICTIONARY RUNS THRU; 
-        NEEDS TO BE READ AND HASHED WORD BY WORD (MIN 6:23 OF VIDEO) */
+        int index = 0;
+        strcpy(w->word, word); 
         insert(&w);
         index++;
+        node *w = malloc(sizeof(node)); /* TO CONFIRM: Is this right? Using same function iterably with the intent of creating a new block each time */
+        return true;
     }
-    return true;
+    free(word);
+    free(w);
 
 
     bool insert (node *w)
@@ -74,9 +65,6 @@ bool load(const char *dictionary)
         table[index] = w; 
         return true;
     }
-
-    
-}
 
 
 // Hashes word to a number
@@ -94,9 +82,6 @@ unsigned int hash(const char *word)
         x = tolower(x);
     }
     return hash_value % N;
-    
-
-
 
     /* return 0; not sure how to have hash() return 0 yet */
 }
@@ -105,8 +90,21 @@ unsigned int hash(const char *word)
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    // TODO
-    return false;
+    int index = hash(word);
+    node *tmp = table[index];
+    
+    while(tmp != NULL && strncmp(tmp->word, word, LENGTH) != 0)
+    {
+        tmp = tmp->next;
+    }
+    if(tmp != NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false; /* clean this up once finished; figure out the shortest form acceptable */
+    }
 }
 
 
